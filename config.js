b2a72d3e319a69056484b0cf291c171e748aa839
qSaltyyy <63613884+qSaltyyy@users.noreply.github.com>
2020-04-14 17:45:00 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  restockDelay: 1000,
+  monitorDelay: 1000,
+  errorDelay: 1500,
+  proxies: {
+    useProxies: false,
+    proxyFile: "proxies.txt"
+  },
+  discord: {
+    webhook: "https://discordapp.com/api/webhooks/699348447546966046/G_po65BQwPBxlTFii3ZOdEA-cbw4APRAjGmiDT7t6otR_kgIRVNDXVpjsCrChO5ZOglZ",
+    webhookFooter: "Written by @Salty",
+    webhookColor: 7785669
+  }
+};
